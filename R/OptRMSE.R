5dca5de1e6c6cdef07f9dd2157ea7df8a145ca4f
Zachary Mayer <zach.mayer@gmail.com>
2013-03-05 10:45:49 -0500

    Removed model-based optimizers
    Removed model-based optimizers such as lmOpt and glmnetOpt.  These will
    be included in the "stacking" optimizers.

--- R/OptRMSE.R
+++ R/OptRMSE.R
@@ -22,32 +22,3 @@ greedOptRMSE <- function(X, Y, iter = 100L){
   }
   return(weights)
 }
-
-
-#' TODO
-#' @param X
-#' @param Y
-#' @export
-lmOpt <- function(X, Y){
-  require('caret')
-  model <- lm.fit(X, Y)
-  weights <- coef(model)
-  names(weights) <- NULL
-  return(weights)
-}
-
-
-#' TODO
-#' @param X
-#' @param Y
-#' @export
-glmnetOpt <- function(X, Y){
-  require('caret')
-  model <- train(X, Y, method='glmnet', 
-                 trControl=trainControl(method='cv', number=16),
-                 tuneGrid=expand.grid(.alpha=c(0, .5, 1), .lambda=seq(0, 5, length=25))
-  )
-  weights <- as.numeric(coef(model$finalModel, model$bestTune$.lambda))[-1]
-  names(weights) <- NULL
-  return(weights)
-}
