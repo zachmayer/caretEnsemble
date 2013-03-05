7e282fd629302268909c4024f624479b21135f3a
Zachary Mayer <zach.mayer@gmail.com>
2013-03-05 12:20:19 -0500

    Added more tests

--- inst/test/test_helper_functions.R
+++ inst/test/test_helper_functions.R
@@ -1,5 +1,16 @@
 
+#TODO: add tests for every helper function
+
 context("Do the helper functions work?")
+
+test_that("We can make the predobs matrix", {
+  data('models_reg')
+  out <- makePredObsMatrix(models_reg)
+  expect_that(out, is_a("list"))
+  expect_true(length(out$obs)==150)
+  expect_true(all(dim(out$preds)==c(150, 3)))
+
+})
 
 test_that("We can multiPredict", {
   data('models_reg')
