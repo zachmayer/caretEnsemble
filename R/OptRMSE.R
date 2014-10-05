1f77143da9ce0a39d6bb0455d4f709d10acec578
Zach Mayer <zach.mayer@gmail.com>
2014-10-05 17:47:27 -0400

    Ripping code out

--- R/OptRMSE.R
+++ R/OptRMSE.R
@@ -23,28 +23,3 @@ greedOptRMSE <- function(X, Y, iter = 100L){
   }
   return(weights)
 }
-
-#' Quadratic optimization of the reduced mean square error
-#' @description This algorithm optimizes the RMSE for regression models to avoid convex combinations
-#' @param X the matrix of predictors
-#' @param Y the dependent variable
-#' @author Sam Lendle
-#' @return A numeric of the weights for each model
-#' @export
-qpOptRMSE <- function(x, y) {
-  weights <- rep(0, ncol(x))
-  lin_indep_cols <- unique(cummax(qr(x)$pivot))
-
-  if (length(lin_indep_cols) < ncol(x)) {
-    message("Some model predictions are identical or lin. dep. to other models and are assigned zero weights")
-    #or warning
-  }
-  x <- x[, lin_indep_cols]
-
-  D <- crossprod(x)
-  d <- crossprod(x, y)
-  A <- cbind(rep(1, ncol(x)), diag(ncol(x)))
-  bvec <- c(1, rep(0, ncol(x)))
-  weights[lin_indep_cols] <- quadprog::solve.QP(Dmat=D, dvec=d, Amat=A, bvec=bvec, meq=1)$solution
-  return(round(weights*100))
-}
