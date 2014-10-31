0d40ccb2521020f14599ba544e4c284e84cacff2
Jared Knowles <jknowles@gmail.com>
2014-10-31 10:15:48 -0500

    fixing caretList tests

--- tests/testthat/test-caretList.R
+++ tests/testthat/test-caretList.R
@@ -26,15 +26,7 @@ test1 <- buildModels(methodList = c("knn", "glm"), control = myControl,
                      y = train[, "Class"], metric = "ROC")
 
 
-# Simple 4 method list
-test2 <- buildModels(methodList = c("knn", "glm", "treebag"), control = myControl,
-                     x = train[, -23],
-                     y = train[, "Class"], metric = "ROC")
 
-test3 <- buildModels(methodList = c("lda", "knn", "glm"),
-                     control = myControl,
-                     x = train[, -23],
-                     y = train[ , "Class"], metric = "ROC")
 
 ## make seeds fail
 # fitControl <- trainControl(method='cv', number = 5, savePredictions = TRUE,
@@ -49,18 +41,33 @@ test3 <- buildModels(methodList = c("lda", "knn", "glm"),
 context("Test that buildModels makes model lists")
 test_that("buildModels returns a list", {
   expect_is(test1, "list")
-  expect_is(test2, "list")
-  expect_is(test3, "list")
-})
 
+})
 
 context("Test that buildModels can be ensembled")
 test_that("buildModels objects can be ensembled", {
   expect_is(caretEnsemble(test1), "caretEnsemble")
-  expect_is(caretEnsemble(test2), "caretEnsemble")
-  expect_is(caretEnsemble(test3), "caretEnsemble")
+
 })
 
+
+test_that("longer tests", {
+  skip_on_cran()
+  test2 <- buildModels(methodList = c("knn", "glm", "rf"), control = myControl,
+                       x = train[, -23],
+                       y = train[, "Class"], metric = "ROC")
+
+  test3 <- buildModels(methodList = c("gaussprLinear", "knn", "glm"),
+                       control = myControl,
+                       x = train[, -23],
+                       y = train[ , "Class"], metric = "ROC")
+
+  expect_is(test2, "list")
+  expect_is(test3, "list")
+  expect_is(caretEnsemble(test2), "caretEnsemble")
+  expect_is(caretEnsemble(test3), "caretEnsemble")
+
+})
 
 context("Test that buildModels perserves user specified functions")
 
@@ -70,12 +77,12 @@ myControl = trainControl(method = "cv", number = 3, repeats = 1,
                          returnData = TRUE, verboseIter = FALSE)
 
 
-test2 <- buildModels(methodList = c("knn", "nb", "glm"), control = myControl,
+test2 <- buildModels(methodList = c("knn", "rf", "glm"), control = myControl,
                      x = train[, -23],
                      y = train[, "Class"], tuneLength = 4, baseSeed = 3252,
                      metric = "Accuracy")
 
-test1 <- buildModels(methodList = c("knn", "lda", "glm"), control = myControl,
+test1 <- buildModels(methodList = c("knn", "rf", "glm"), control = myControl,
                      x = train[, -23],
                      y = train[, "Class"], tuneLength = 7, baseSeed = 3252,
                      metric = "Kappa")
@@ -106,43 +113,42 @@ context("Users can pass a custom tuneList")
 # User specifies methods and tuning parameters specifically using a tuneList
 tuneTest <- list(rf=list(tuneGrid=data.frame(.mtry=c(2,4,8,1))),
                  knn=list(tuneLength=9),
-                 lda2=list(tuneLength=6))
+                 gaussprRadial=list(tuneGrid=data.frame(.sigma = c(0.1, 0.2, 0.4))))
 
 # Simple with mix of data.frame and tuneLength
 
-test2a <- buildModels(tuneList = tuneTest, control = myControl,  x = train[, -23],
-                     y = train[, "Class"])
 
-myEns2a <- caretEnsemble(test2a)
 
 test_that("User tuneTest parameters are respected and model is ensembled", {
+  skip_on_cran()
+  test2a <- buildModels(tuneList = tuneTest, control = myControl,  x = train[, -23],
+                        y = train[, "Class"])
+  myEns2a <- caretEnsemble(test2a)
   expect_is(myEns2a, "caretEnsemble")
   expect_is(test2a, "list")
   expect_equal(nrow(test2a[[1]]$results), 4)
   expect_equal(nrow(test2a[[2]]$results), 9)
-  expect_equal(nrow(test2a[[3]]$results), 1)
+  expect_equal(nrow(test2a[[3]]$results), 3)
 })
 
 context("More complex with multidimensional tuneGrid and NULL tuneLengths")
 
-tuneTest2 <- list(glm = list(NULL), lda2 = list(tuneLength = 4),
-                  knn = list(tuneLength = 2),
-                  nb = list(tuneGrid = expand.grid(.fL = c(0, 0.4, 0.8, 1, 2),
-                                                  .usekernel = c(TRUE, FALSE))))
 
-
-test3a <- buildModels(tuneList = tuneTest2, control = myControl,  x = train[, -23],
-                      y = train[, "Class"])
-
-myEns3a <- caretEnsemble(test3a)
 
 test_that("User tuneTest parameters are respected and model is ensembled", {
+  skip_on_cran()
+  tuneTest2 <- list(glm = list(tuneLength = 1),
+                    knn = list(tuneLength = 2),
+                    gaussprPoly = list(tuneGrid = expand.grid(.degree = c(1, 2, 3),
+                                                     .scale = c(.001, .01, .1))))
+  test3a <- buildModels(tuneList = tuneTest2, control = myControl,  x = train[, -23],
+                        y = train[, "Class"])
+    myEns3a <- caretEnsemble(test3a)
   expect_is(myEns3a, "caretEnsemble")
   expect_is(test3a, "list")
   expect_equal(nrow(test3a[[1]]$results), 1)
-  expect_equal(nrow(test3a[[2]]$results), 1)
-  expect_equal(nrow(test3a[[3]]$results), 2)
-  expect_equal(nrow(test3a[[4]]$results), 10)
+  expect_equal(nrow(test3a[[2]]$results), 2)
+  expect_equal(nrow(test3a[[3]]$results), 9)
 })
 
 
