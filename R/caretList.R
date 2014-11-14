d5f8ffa11a8ccd4d1056c0965a07dbc777b509aa
Zach Mayer <zach.mayer@gmail.com>
2014-11-14 15:58:59 -0500

    Initial re-write of buildModels
    Major re-factor, trying to remove all the places where we need to set
    seeds ourselves.

--- R/caretList.R
+++ R/caretList.R
@@ -2,49 +2,17 @@
 #'
 #' @param x a list of tuning parameters and methods passed by the user
 #' @return A simple list of the tuning parameters
-tuneExtract <- function(x){
-  tuneType <- paste(names(x))
-  tunePar <- x[[1]]
-  return(list(tuneType = tuneType, tunePar = tunePar))
+tuneCheck <- function(x){
+  warning('NOT IMPLEMENTED')
+  return(invisible(NULL))
 }
-
-#' Create uniform seeds across model fits to allow for ensembling
-#'
-#'
-#' @param ctrl a \code{\link{trainControl}} object passed by the user
-#' @param M the maximum number of resamples necessary
-#' @return A \code{\link{trainControl}} object with a new slot, seeds
-#' @details Currently the seed structure is determined with the length of the
-#' seed list being number * repeats +1 and the length of all vectors B -1 being
-#' 20 * tuneLength^2 with the final vector being a single seed
-setSeeds <- function(ctrl, M){
-  # M is the square of the tune-length
-  B <- ctrl$number * ctrl$repeats
-  mseeds <- vector(mode = "list", length = B + 1)
-  if(length(M) > 1){M <- max(M)}
-  M <- M * 20 # hack for arbitrary scalar
-  for(i in 1:B) mseeds[[i]] <- .Random.seed[1:M] # is this the best way?
-  mseeds[[B+1]] <- .Random.seed[1]
-  ctrl$seeds <- mseeds
-  return(ctrl)
+methodCheck <- function(x){
+  warning('NOT IMPLEMENTED')
+  return(invisible(NULL))
 }
-
-#' Helper function to extract tune grids from arguments passed to tuneList
-#'
-#' Returns the tuneGrids of the tuneList
-#'
-#' @param x list of tuning parameters for train objects
-#' @return The maximum length of any tuneGrid in the list
-tmpExtract <- function(x){
-  if(is.null(names(x))){
-    tmp <- 0
-  } else if(names(x) == "tuneGrid"){
-    tmp <- nrow(x$tuneGrid)
-  } else{
-    tmp <- max(x$tuneLength)
-  }
-  #tmp <- ifelse(length(y) > 1, length(y), max(y))
-  return(tmp)
+indexCheck <- function(x){
+  warning('NOT IMPLEMENTED')
+  return(x)
 }
 
 #' Create a list of several train models from the caret package
@@ -52,62 +20,58 @@ tmpExtract <- function(x){
 #' Build a list of train objects suitable for ensembling using the \code{\link{caretEnsemble}}
 #' function.
 #'
-#' @param methodList a character vector of caret models to ensemble.
-#' @param control a \code{\link{trainControl}} object
-#' @param x a matrix of predictors
-#' @param y the dependent variable being predicted
-#' @param tuneList optional, a list of the length of \code{methodList} with the tuning parameters for each method
-#' @param baseSeed optional, to preserve reproducibility, a base seed to be used in the resampling randomization
-#' @param tuneLength optional, the length of tuning to be done
-#' @param ... additional arguments to pass to \code{\link{train}}
+#' @param ... arguments to pass to \code{\link{train}}.  These arguments will determine which train method gets dispatched.
+#' @param trControl a \code{\link{trainControl}} object.  We are going to intercept this object check that it has the "index" slot defined, and define the indexes if they are not.
+#' @param methodList optional, a character vector of caret models to ensemble.  One of methodList or tuneList must be specified.
+#' @param tuneList optional, a NAMED list of the length of \code{methodList} with model-specific arguments to pass to train.  The can be arguments for the train function (e.g. tuneLength=6) or arguments passed through train to the modeling funcion (e.g. verbose=FALSE for a \code{\link{gbm}} model).  The names in the tuneList must match the methods in methodList, but do not need to be in the same order.  One of methodList or tuneList must be specified.
 #' @return A list of \code{\link{train}} objects
 #' @import caret
 #' @export
-buildModels <- function(methodList, control, x, y, tuneList = NULL, baseSeed = NULL, tuneLength = NULL, ...) {
-  if(!missing(tuneList)){
+buildModels <- function(..., trControl = trainControl(), methodList = NULL, tuneList = NULL) {
+
+  #Decide how we're going to fit models
+  if(is.null(tuneList) & is.null(methodList)){
+    stop('Please either define a methodList or tuneList')
+  }
+  if((!is.null(tuneList)) & (!is.null(methodList))){
+    warning('Both tuneList and methodList defined.  Ignoring methodList')
+  }
+
+  #Make sure tuneList is valid
+  if(!is.null(tuneList)){
     methodList <- names(tuneList)
-    suppressWarnings(tmp <- lapply(tuneList, tmpExtract))
-    tmp2 <- lapply(methodList, function(x) length(unique(modelLookup(x)$parameter)))
-    tmp <- as.numeric(tmp)
-    tmp2 <- as.numeric(tmp2)
-    M <- max(tmp^tmp2)
-  } else if(missing(tuneList)){
-    tl <- ifelse(missing(tuneLength), 6, tuneLength)
-    M <- tl ^ 2
+    tuneCheck(tuneList)
   }
-  seedFlag <- length(control$seeds)
-  if(seedFlag < 2){ # if custom seeds are already present in trainControl, keep them
-    if(missing(tuneList)){ctrl <- setSeeds(control, M = M)}
-    else {
-      ctrl <- setSeeds(control, M = M)
-    }
-  }
-  #
-  if(!missing(tuneList)){methodList <- names(tuneList)}
-  if(missing(baseSeed)){baseSeed <- .Random.seed[1]}
-  #
-  modelList <- sapply(methodList, function(x) NULL)
 
-  for(i in methodList){
-    set.seed(baseSeed) # so splits are same across resamples
-    if(missing(tuneList)){
-      modelList[[i]] <- train(x = x, y=y, method = i, trControl = ctrl,
-                              tuneLength = tl, ...)
-    } else {
-      tmpTune <- tuneExtract(tuneList[[i]])
-      if(length(tmpTune$tuneType)==0){
-        tmpTune$tuneType <- "tuneLength"
-      }
-      if(is.null(tmpTune$tunePar)){
-        warning("No tuning parameters provided, using default of tuneLength = 6")
-        tmpTune$tunePar <- 6
-      }
-      modelList[[i]] <- eval(parse(text = paste("train(x = x, y=y, method = i,
-                                                trControl = ctrl,", tmpTune$tuneType,
-                                                " = tmpTune$tunePar, ...)")))
-      tmpTune <- NULL; Tune <- NULL
+  #Check that the methods are valid
+  methodCheck(methodList)
+
+  #Define the index slot of trControl if it is missing
+  trControl <- indexCheck(trControl)
+
+  #Squish trControl back onto the global arguments list
+  global_args <- list(...)
+  global_args[['trControl']] <- trControl
+
+  #If the tuneList is missing, fit each model with 100% default caret params
+  modelList <- lapply(methodList, function(i){
+
+    #Start with global args
+    model_args <- global_args
+
+    #Add tuneList args, if needed
+    if(!is.null(tuneList)){
+      model_args <- c(model_args, tuneList[[i]])
     }
 
-  }
+    #Finally, add method.
+    model_args[['method']] <- i
+
+    #We now have a complete args list for train, so we can dispatch with do.cal
+    model <- do.call(train, model_args)
+    return(model)
+  })
+
+  names(modelList) <- methodList
   return(modelList)
 }
