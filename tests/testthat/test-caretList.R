80e3b1c254cf4da078f014382a6e1f41856838be
Zach Mayer <zach.mayer@gmail.com>
2014-11-16 20:48:10 -0500

    Added S3 methods for caretStack
    print/plot/summary/dotplot

--- tests/testthat/test-caretList.R
+++ tests/testthat/test-caretList.R
@@ -6,6 +6,7 @@ library('randomForest')
 library('rpart')
 library('gbm')
 library('kernlab')
+library('nnet')
 train <- twoClassSim(
   n = 1000, intercept = -8, linearVars = 3,
   noiseVars = 10, corrVars = 4, corrValue = 0.6)
