0b5624e91a6b13456ecce735147a244272e77afe
Zach Mayer <zach.mayer@gmail.com>
2014-12-19 10:41:51 -0500

    adding some more tests back in

--- tests/testthat/test-caretList.R
+++ tests/testthat/test-caretList.R
@@ -24,3 +24,130 @@ test_that("caretModelSpec returns valid specs", {
   expect_equal(length(tuneList), 4)
   expect_equal(sum(duplicated(names(tuneList))), 0)
 })
+
+###############################################
+context("We can fit models with a mix of methodList and tuneList")
+################################################
+test_that("caretModelSpec returns valid specs", {
+  skip_on_cran()
+  rm(list=ls(all=TRUE))
+  gc(reset=TRUE)
+
+  myList <- list(
+    rpart=caretModelSpec(method='rpart', tuneLength=10),
+    rf=caretModelSpec(method='rf', tuneGrid=data.frame(mtry=2))
+  )
+  expect_warning({
+    test <- caretList(
+      x = iris[,1:3],
+      y = iris[,4],
+      methodList = c("knn", "glm"),
+      tuneList=myList
+    )
+  })
+  expect_is(test, "caretList")
+  expect_is(caretEnsemble(test), "caretEnsemble")
+  expect_equal(length(test), 4)
+  methods <- sapply(test, function(x) x$method)
+  names(methods) <- NULL
+  expect_equal(methods, c('rpart', 'rf', 'knn', 'glm'))
+})
+
+################################################
+context("We can work around bad models")
+################################################
+
+test_that("We can work around bad models", {
+  skip_on_cran()
+  rm(list=ls(all=TRUE))
+  gc(reset=TRUE)
+
+  myList <- list(
+    rpart=caretModelSpec(method='rpart', tuneLength=3),
+
+    #Forgot to specify interaction.depth, shrinkage
+    gbm=caretModelSpec(method='gbm', tuneGrid=data.frame(
+      n.trees=-100,
+      interaction.depth=-100,
+      shrinkage=-100))
+  )
+  suppressWarnings({
+    expect_error({
+      test1 <- caretList(
+        x = iris[,1:3],
+        y = iris[,4],
+        tuneList=myList,
+        continue_on_model_fail=FALSE
+      )
+    })
+    test <- caretList(
+      x = iris[,1:3],
+      y = iris[,4],
+      tuneList=myList,
+      continue_on_model_fail=TRUE
+    )
+  })
+
+  expect_is(test, "caretList")
+  expect_is(caretEnsemble(test), "caretEnsemble")
+  expect_equal(length(test), 1)
+  methods <- sapply(test, function(x) x$method)
+  names(methods) <- NULL
+  expect_equal(methods, c('rpart'))
+})
+
+################################################
+context("We can handle different CV methods")
+################################################
+
+test_that('CV methods work', {
+  for(m in c(
+    'boot',
+    'adaptive_boot',
+    'cv',
+    'adaptive_cv',
+    'LGOCV',
+    'adaptive_LGOCV')
+  ){
+    skip_on_cran()
+    rm(list=ls(all=TRUE))
+    gc(reset=TRUE)
+
+    myControl = trainControl(
+      method = m,
+      number = 7,
+      repeats = 1,
+      p = 0.75,
+      savePrediction = TRUE,
+      returnResamp = "final",
+      returnData = FALSE,
+      verboseIter = FALSE)
+
+    suppressWarnings({
+      suppressMessages({
+        models <- caretList(
+          x = iris[,1:3],
+          y = iris[,4],
+          trControl = myControl,
+          tuneLength=2,
+          methodList = c('rpart', 'rf')
+        )
+      })
+    })
+    sink <- sapply(models, expect_is, class='train')
+
+    suppressWarnings({
+      suppressMessages({
+        ens <- caretEnsemble(models, iter=10)
+      })
+    })
+
+    expect_is(ens, 'caretEnsemble')
+
+    suppressMessages({
+      ens <- caretStack(models, method='glm', trControl=trainControl(number=2))
+    })
+
+    expect_is(ens, 'caretStack')
+  }
+})
