c94366121b4e1a01e9940b49650bd3e4b5e746e6
Jason Cohen <JasonCEC@gastrograph.com>
2016-01-25 12:53:32 -0500

    created S3 generic function for c.train
    fixed oversight in c.caretList function which would not apply as a generic method for objects of class train.
    
    Updated documentation of both c.train and c.caretList

--- R/S3GenericExtenstions.R
+++ R/S3GenericExtenstions.R
@@ -1,9 +1,10 @@
 ## Extensions to generic S3 functions ##
 # (someone should spell check this)
 
-#' @title S3 definition for concatenating models
+#' @title S3 definition for concatenating caretList
 #'
-#' @description take two lists of models and concatenat them for future Ensamble'ing
+#' @description take N objects of class caretList and concatenat them into a larger object of
+#' class caretList for future Ensamble'ing
 #'
 #' @param ... the objects of class caretList or train to bind into a caretList
 #' @return a \code{\link{caretList}} object
@@ -31,6 +32,53 @@
 #' }
 #'
 c.caretList <- function(...) {
+
+  new_model_list <- unlist(lapply(list(...), function(x) {
+    if(class(x)[1] != "caretList") {
+      if(class(x)[1] != "train") stop("class of modelList1 must be 'caretList' or 'train'")
+
+      ## assuming this is a single train object
+      x <- list(x)
+      names(x) <- x[[1]]$method
+      return(x)
+    } else {
+      return(x)
+    }
+  }), recursive = FALSE)
+
+  ## Make sure names are unique
+  names(new_model_list) <- paste0(names(new_model_list), 1:length(new_model_list))
+
+  ## reset the class to caretList
+  class(new_model_list) <- "caretList"
+
+  return(new_model_list)
+}
+
+
+#' @title S3 definition for concatenating train objects
+#'
+#' @description take N objects of class train and concatenat into an object of class caretList for future Ensamble'ing
+#'
+#' @param ... the objects of class train to bind into a caretList
+#' @return a \code{\link{caretList}} object
+#' @export
+#' @examples
+#' \dontrun{
+#' rpartTrain <- train(Class ~ .,
+#'                     data=Sonar,
+#'                     trControl = ctrl1,
+#'                     method='rpart')
+#'
+#' rfTrain <- train(Class ~ .,
+#'                  data=Sonar,
+#'                  trControl = ctrl1,
+#'                  method='rf')
+#'
+#'  bigList <- c(model_list1, model_list2)
+#' }
+#'
+c.train <- function(...) {
 
   new_model_list <- unlist(lapply(list(...), function(x) {
     if(class(x)[1] != "caretList") {
