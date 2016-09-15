3730e7553657500fee57ba56443cb5c63556ab5e
washcycle <matthew.landowski@gmail.com>
2016-09-15 18:13:24 -0500

    updated test, fixed lintr errors, made as.caretList more robust and future proof

--- tests/testthat/test-caretStack.R
+++ tests/testthat/test-caretStack.R
@@ -116,13 +116,13 @@ test_that("Failure to calculate se occurs gracefully", {
 })
 
 test_that("Test na.action pass through", {
-  set.seed(96367)
+  set.seed(1337)
 
   ens.reg <- caretStack(models.reg[2:3], method="lm")
 
-  X.reg.na <- X.reg
+  X_reg_na <- X.reg
   # introduce random NA values into a column
-  X.reg.na[sample(1:nrow(X.reg.na), 20), sample(1:ncol(X.reg.na)-1, 1)] <- NA
+  X_reg_na[sample(1:nrow(X_reg_na), 20), sample(1:ncol(X_reg_na)-1, 1)] <- NA
 
   expect_warning(pred.reg <- predict(ens.reg, newdata = X.reg.na, na.action = na.pass))
   expect_length(pred.reg, nrow(X.reg.na))
