a3f4762f8b485338897a89054c6f1a68b09edb69
Zachary Deane-Mayer <zach.mayer@gmail.com>
2023-09-18 15:47:29 -0400

    add script to verify cran submissions

---
+++ check_package.R
@@ -0,0 +1,6 @@
+devtools::check(cran=T, remote=T, manual=T, error_on="note")
+devtools::check(cran=F, remote=T, manual=T, error_on="note", force_suggests=T)
+
+devtools::check_win_devel()
+devtools::check_win_release()
+devtools::check_win_oldrelease()
